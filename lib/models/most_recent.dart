5fa84a4e4fd02197a54d63fff26ad3473bc265f5
moosaad <muhammedsaad.x1@gmail.com>
2025-09-09 22:10:27 +0300

    finished most recent suras - memory

--- lib/models/most_recent.dart
+++ lib/models/most_recent.dart
@@ -4,10 +4,12 @@ class MostRecent{
  static List<SuraModel> mostRecentSuras = [];
  static void addMostRecent(SuraModel suraModel){
     mostRecentSuras.add(suraModel);
+    /// how to cache list -> Presistant storage
 
   }
 
   static List<SuraModel> getMostRecentSuras(){
    return mostRecentSuras;
+   /// read data from presistant storage
  }
 }
