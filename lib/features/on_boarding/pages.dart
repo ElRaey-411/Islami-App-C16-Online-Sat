c33e12d076ac7c7ed851fa86c8bea2ec4eeac490
Abdelhady Elraey <abdelhadyelraey411@gmail.com>
2025-09-17 01:55:54 +0300

    onboarding done

---
+++ lib/features/on_boarding/pages.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import '../../core/resources/assets_manager.dart';
+import '../../core/resources/colors_manager.dart';
+
+class Pages {
+  Pages({
+    required this.bodyImage,
+    required this.subtitle,
+    this.description = "",
+  });
+
+  final String bodyImage;
+  final String subtitle;
+  final String description;
+
+  PageViewModel toPageViewModel() {
+    return PageViewModel(
+      title: "",
+      bodyWidget: Column(
+        children: [
+          Image.asset(ImageAssets.islamiLogo),
+           SizedBox(height: 50),
+          Image.asset(bodyImage),
+           SizedBox(height: 60),
+          Text(
+            subtitle,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: ColorsManager.gold,
+            ),
+          ),
+           SizedBox(height: 20),
+          Text(
+            description,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: ColorsManager.gold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
